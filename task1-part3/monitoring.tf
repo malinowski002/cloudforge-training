01b29e002454923dcd1596a4dad8f7bfef2240e0
malinowski002 <kacpermalinowski002@gmail.com>
2026-02-11 13:29:01 +0100

    fix terraform format

--- task1-part3/monitoring.tf
+++ task1-part3/monitoring.tf
@@ -67,7 +67,7 @@ resource "aws_cloudwatch_metric_alarm" "waf_blocks" {
   dimensions = {
     WebACL = module.waf.web_acl_name
     Region = "Global"
-    Rule = "ALL"
+    Rule   = "ALL"
   }
 
   alarm_actions = [aws_sns_topic.alerts.arn]
