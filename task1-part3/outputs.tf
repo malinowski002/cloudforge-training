1bd2dffaf3f88289b144d27895565786e602e855
malinowski002 <kacpermalinowski002@gmail.com>
2026-02-11 12:41:37 +0100

    fix terraform formatting

--- task1-part3/outputs.tf
+++ task1-part3/outputs.tf
@@ -1,4 +1,4 @@
 output "cloudfront_url" {
-    description = "URL of the website"
-    value = "https://${module.cloudfront.distribution_domain_name}"
+  description = "URL of the website"
+  value       = "https://${module.cloudfront.distribution_domain_name}"
 }
--- task1-part3/providers.tf
+++ task1-part3/providers.tf
@@ -1,33 +1,33 @@
-terraform { 
-  cloud { 
-    
-    organization = "cloudforge-training" 
+terraform {
+  cloud {
 
-    workspaces { 
-      name = "cloudforge-training-workspace" 
-    } 
+    organization = "cloudforge-training"
+
+    workspaces {
+      name = "cloudforge-training-workspace"
+    }
   }
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-    region = "eu-north-1"
+  region = "eu-north-1"
 
-    default_tags {
-        tags = {
-            project = "static-website"
-        }
+  default_tags {
+    tags = {
+      project = "static-website"
     }
+  }
 }
 
 provider "aws" {
-  alias = "us_east_1"
+  alias  = "us_east_1"
   region = "us-east-1"
   default_tags {
     tags = {
