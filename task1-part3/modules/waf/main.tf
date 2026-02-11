090281f7ad693acdfed532ee22041580e4a7ffaa
malinowski002 <kacpermalinowski002@gmail.com>
2026-02-11 08:23:57 +0100

    add WAF IP rate limit rule

--- task1-part3/modules/waf/main.tf
+++ task1-part3/modules/waf/main.tf
@@ -25,6 +25,27 @@ resource "aws_wafv2_web_acl" "this" {
         }
     }
 
+    rule {
+        name = "IPRateLimit"
+        priority = 1
+        action {
+            block {}
+        }
+
+        statement {
+            rate_based_statement {
+                limit = 100
+                aggregate_key_type = "IP"
+            }
+        }
+
+        visibility_config {
+            cloudwatch_metrics_enabled = true
+            metric_name = "IPRateLimitMetric"
+            sampled_requests_enabled = true
+        }
+    }
+
     visibility_config {
         cloudwatch_metrics_enabled = true
         metric_name = "static-website-waf"
