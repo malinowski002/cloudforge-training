7f50c2ec9ebf530398f89fbaffacc1e56a24e2e0
malinowski002 <kacpermalinowski002@gmail.com>
2026-02-13 14:26:45 +0100

    fix run platform

--- ecs-app-part2/modules/ecs-task-definition/main.tf
+++ ecs-app-part2/modules/ecs-task-definition/main.tf
@@ -14,4 +14,9 @@ resource "aws_ecs_task_definition" "this" {
       Name = var.family
     }
   )
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
 }
