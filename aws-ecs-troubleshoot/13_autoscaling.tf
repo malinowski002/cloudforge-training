4d64759e662ed7eb9211e9890d79dd49694d87c4
malinowski002 <kacpermalinowski002@gmail.com>
2026-02-17 18:57:35 +0100

    add step scaling policies and cloudwatch alarms

--- aws-ecs-troubleshoot/13_autoscaling.tf
+++ aws-ecs-troubleshoot/13_autoscaling.tf
@@ -9,3 +9,88 @@ resource "aws_appautoscaling_target" "web" {
   max_capacity       = var.web_max_capacity
 }
 
+# ---------------------
+# Step scaling policies
+# ---------------------
+
+resource "aws_appautoscaling_policy" "web_scale_out" {
+  name               = "${var.name}-web-scale-out"
+  policy_type        = "StepScaling"
+  service_namespace  = aws_appautoscaling_target.web.service_namespace
+  resource_id        = aws_appautoscaling_target.web.resource_id
+  scalable_dimension = aws_appautoscaling_target.web.scalable_dimension
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+    step_adjustment {
+      metric_interval_lower_bound = 20
+      scaling_adjustment          = 2
+    }
+  }
+}
+
+resource "aws_appautoscaling_policy" "web_scale_in" {
+  name               = "${var.name}-web-scale-in"
+  policy_type        = "StepScaling"
+  service_namespace  = aws_appautoscaling_target.web.service_namespace
+  resource_id        = aws_appautoscaling_target.web.resource_id
+  scalable_dimension = aws_appautoscaling_target.web.scalable_dimension
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 120
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
+
+# --------------------------------------
+# CloudWatch Alarms for scaling policies
+# --------------------------------------
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_high" {
+  alarm_name          = "${var.name}-web-cpu-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 70
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.this.name
+    ServiceName = aws_ecs_service.web.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.web_scale_out.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_low" {
+  alarm_name          = "${var.name}-web-cpu-low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 3
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 30
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.this.name
+    ServiceName = aws_ecs_service.web.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.web_scale_in.arn]
+}
