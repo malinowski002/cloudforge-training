a67755b026266b47bc79ed2e4da68c6dbdda9b9a
malinowski002 <kacpermalinowski002@gmail.com>
2026-02-17 11:08:20 +0100

    fix pipeline

--- task1-part3/oidc.tf
+++ task1-part3/oidc.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["2b18947a6a9fc7764fd8b5fb18a863b0c6dac24f"]
 }
 
 resource "aws_iam_role" "github_tf_plan" {
