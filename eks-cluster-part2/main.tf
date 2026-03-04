14d093d61505903313898c67e38e01d297062595
malinowski002 <kacpermalinowski002@gmail.com>
2026-03-04 08:59:34 +0100

    upgrade ami and enable public access to EKS

--- eks-cluster-part2/main.tf
+++ eks-cluster-part2/main.tf
@@ -3,7 +3,10 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = "minimal-eks-cluster"
-  cluster_version = "1.29"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
@@ -14,6 +17,7 @@ module "eks" {
       max_size       = 2
       desired_size   = 1
       instance_types = ["t3.medium"]
+      ami_type = "AL2023_x86_64"
     }
   }
 }
