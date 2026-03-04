a674e8a10022f6e38addde3eeba67a2bdf4ae315
malinowski002 <kacpermalinowski002@gmail.com>
2026-03-04 07:43:15 +0100

    initial commit

---
+++ eks-cluster-part2/main.tf
@@ -0,0 +1,19 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "minimal-eks-cluster"
+  cluster_version = "1.27"
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  eks_managed_node_groups = {
+    default = {
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+}
