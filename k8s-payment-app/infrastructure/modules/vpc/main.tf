3eccdb934800dce029c79676a95b2d0de11f00e3
malinowski002 <kacpermalinowski002@gmail.com>
2026-03-12 09:57:47 +0100

    add ngw, eip and private route table

--- k8s-payment-app/infrastructure/modules/vpc/main.tf
+++ k8s-payment-app/infrastructure/modules/vpc/main.tf
@@ -58,3 +58,41 @@ resource "aws_route_table_association" "public" {
     subnet_id = each.value.id
     route_table_id = aws_route_table.public.id
 }
+
+resource "aws_eip" "nat" {
+    domain = "vpc"
+    tags = {
+        Name = "${var.name}-nat-eip"
+    }
+}
+
+resource "aws_nat_gateway" "this" {
+    subnet_id = aws_subnet.public[var.public_subnets[0]].id
+    allocation_id = aws_eip.nat.id
+
+    tags = {
+        Name = "${var.name}-nat-gateway"
+    }
+
+    depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.this.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.this.id
+    }
+
+    tags = {
+        Name = "${var.name}-private-rt"
+    }
+}
+
+resource "aws_route_table_association" "private" {
+    for_each = aws_subnet.private
+
+    subnet_id = each.value.id
+    route_table_id = aws_route_table.private.id
+}
