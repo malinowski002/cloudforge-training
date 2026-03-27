d229bee0400e3b676bb484eb914c2bc104ec4d1d
malinowski002 <kacpermalinowski002@gmail.com>
2026-03-27 11:29:36 +0100

    add monitoring dashboard

---
+++ gcp-static-website/modules/monitoring/main.tf
@@ -0,0 +1,75 @@
+resource "google_monitoring_dashboard" "default" {
+    dashboard_json = jsonencode({
+      displayName = "Static Website - LB Monitoring"
+      mosaicLayout = {
+        columns = 12
+        tiles = [
+          {
+            width  = 4
+            height = 4
+            widget = {
+              title = "Total Requests"
+              xyChart = {
+                dataSets = [{
+                  timeSeriesQuery = {
+                    timeSeriesFilter = {
+                      filter = "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\""
+                      aggregation = {
+                        alignmentPeriod    = "60s"
+                        perSeriesAligner   = "ALIGN_RATE"
+                        crossSeriesReducer = "REDUCE_SUM"
+                      }
+                    }
+                  }
+                }]
+              }
+            }
+          },
+          {
+            xPos   = 4
+            width  = 4
+            height = 4
+            widget = {
+              title = "4xx Rate"
+              xyChart = {
+                dataSets = [{
+                  timeSeriesQuery = {
+                    timeSeriesFilter = {
+                      filter = "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\" metric.labels.response_code_class=\"400\""
+                      aggregation = {
+                        alignmentPeriod    = "60s"
+                        perSeriesAligner   = "ALIGN_RATE"
+                        crossSeriesReducer = "REDUCE_SUM"
+                      }
+                    }
+                  }
+                }]
+              }
+            }
+          },
+          {
+            xPos   = 8
+            width  = 4
+            height = 4
+            widget = {
+              title = "5xx Rate"
+              xyChart = {
+                dataSets = [{
+                  timeSeriesQuery = {
+                    timeSeriesFilter = {
+                      filter = "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\" metric.labels.response_code_class=\"500\""
+                      aggregation = {
+                        alignmentPeriod    = "60s"
+                        perSeriesAligner   = "ALIGN_RATE"
+                        crossSeriesReducer = "REDUCE_SUM"
+                      }
+                    }
+                  }
+                }]
+              }
+            }
+          }
+        ]
+      }
+    })
+  }
